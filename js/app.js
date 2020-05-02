299a121ea97be28e4d2369a8c3350a18ce5952c4
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2020-05-02 06:26:40 +0100

    create quiz question

---
+++ js/app.js
@@ -0,0 +1,49 @@
+const quiz = [
+   {
+      question: 'What is Node.JS?',
+      options: [
+         'Node.js is a web server.', 
+         'Node.js is a JavaScript framework', 
+         'Node.js is a JavaScript based framework/platform built on Google Chrome\'s JavaScript V8 Engine.',
+         'None of the above'
+      ],
+   },
+   {
+      question: 'Which of the following command will show version of npm?',
+      options: [
+         '$ npm --version.',
+         '$ node --version.',
+         '$ npm getVersion.',
+         '$ node getVersion.'
+      ]
+   },
+   {
+      question: 'Which of the following command will show version of npm?',
+      options: [
+         'console.log(\'total memory : \' + os.totalMem() + " bytes.");',
+         'console.log(\'total memory : \' + os.totalMemory() + " bytes.");',
+         'console.log(\'total memory : \' + os.getTotalMemory() + " bytes.");',
+         'None of the above.'
+      ]
+   },
+   {
+      question: 'Which of the following module do you need for cryptographic functionality?',
+      options: [
+         'cryptic',
+         'cryptography',
+         'cryptoc',
+         'crypto'
+      ]
+   },
+  
+   {
+      question: 'Where is captured values are populated regarding route parameters?',
+      options: [
+         'req.locals object',
+         'req.params object',
+         'req.parameters object',
+         'req.params object'
+      ]
+   }
+  
+]
